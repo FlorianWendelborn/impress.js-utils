73bb93cc65ab1e712b6b7e9adc32211c59390f5a
FlorianWendelborn <florianwendgithub@googlemail.com>
2013-11-26 22:40:12 +0100

    added hotkey menu

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ put utils.js in your presentation folder and append this to your HTML:
     <script src="./utils.js" type="text/javascript"></script>
     <script type="text/javascript">var utils = new ImpressUtilities();</script>
 
-##menu
+##keyboard menu
 ###description
 This will create keyboard interactivity. The following example will associate t with the slide #title, c with #criteria ...
 ###example
@@ -18,3 +18,9 @@ This will create keyboard interactivity. The following example will associate t 
         'c':'criteria',
         's':'structure'
     });
+
+##menu
+###description
+This will enable a menu that displays keyboard commands when pressing a hotkey. When the menu is active it will be given the CSS class active.
+##example
+    utils.enableMenu(document.getElementById('utils-menu'), 1);
--- utils.js
+++ utils.js
@@ -1,13 +1,62 @@
+/*
+	impress.js-utils by Florian Wendelborn
+	GitHub: https://github.com/FlorianWendelborn/impress.js-utils
+*/
+
 function ImpressUtilities () {
 	var data = {
-		'keyboard':{}
+		'keyboard':{},
+		'menu':{
+			'key':false,
+			'element':false,
+			'visible':false
+		}
 	}
 	window.onkeypress = function (e) {
-		if (data.keyboard[String.fromCharCode(e.charCode)]) {
-			impress().goto(data.keyboard[String.fromCharCode(e.charCode)]);
+		var key = String.fromCharCode(e.charCode);
+		if (data.menu.key == key) {
+			if (data.menu.visible) {
+				removeClass(data.menu.element, 'active');
+				data.menu.visible = false;
+			} else {
+				addClass(data.menu.element, 'active');
+				data.menu.visible = true;
+			}	
+		} else if (data.keyboard[key]) {
+			impress().goto(data.keyboard[key]);
 		}
 	}
 	this.menu = function (d) {
 		data.keyboard = d;
+	}
+	this.enableMenu = function (element, key) {
+		data.menu.element = element;
+		data.menu.key = key;
+
+		var html = '';
+		html += '<table>';
+		for (var i in data.keyboard) {
+			html += '<tr onmousedown="impress().goto(\'' + data.keyboard[i] + '\')"><td>' + i + '</td><td>' + data.keyboard[i] + '</td></tr>';
+		}
+		html += '</table>';
+		data.menu.element.innerHTML = html;
+	}
+	this.disableMenu = function () {
+		data.menu.element = false;
+		data.menu.key = false;
+	}
+
+	var addClass = function (element, className) {
+		element.className += ' ' + className;
+	}
+	var removeClass = function (element, className) {
+		var current = element.className.split(' ');
+		var result = [];
+		for (var i = 0; i < current.length; i++) {
+			if (current[i] != className) {
+				result.push(current[i]);
+			}
+		}
+		element.className = result.join(' ');
 	}
 }
